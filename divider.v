f956c54455a1a73f9ceaef3d858abf56eac2438f
Thomas Ales <alestk@gmail.com>
2023-08-09 10:07:14 -0500

    programmable divider tested

--- divider.v
+++ divider.v
@@ -2,25 +2,38 @@ module divider(
     input pulse_clock,
     input enable,
     input external_reset,
+    // Shift-register specific inputs
+    input sr_data,
+    input sr_data_clock,
+    input sr_div_data_enable,
+    input sr_div_data_reset,
     output divided_clock
 );
 
 reg divided_state = 0;
 reg internal_reset = 0;
-reg [15:0] divide_delay = 16'd1;
+reg [31:0] divide_delay = 32'd1;
 
-reg [15:0] divider_target = 16'd10;
-wire [15:0] compensated_target;
+wire [31:0] divider_target;
+wire [31:0] compensated_target;
 
 wire combined_reset_signal;
-
+wire inverted_serdata_enable;
 wire divider_toggle;
-
+// Internal Counter
 counter32 divider_counter(.count_in(pulse_clock),
                           .count_enable(enable),
                           .count_reset(combined_reset_signal),
                           .count_target(compensated_target),
                           .count_completed(divider_toggle));
+
+// Internal 32b Shift Register to hold the divider target value
+sr32 divider_sr(.serdata(sr_data),
+                .serdata_clock(sr_data_clock),
+                .serdata_enable(sr_div_data_enable),
+                .serdata_reset(sr_div_data_reset),
+                .output_enable(inverted_serdata_enable),
+                .parallel_out(divider_target));
 
 always @(posedge pulse_clock) begin
     // unlatch internal reset signal after one clock cycle.
@@ -34,8 +47,15 @@ always @(posedge pulse_clock) begin
         internal_reset <= 1;
     end
 end
+/************************************
+    Combinatorial Logic Block
+        (not clock based)
+************************************/
 // Combined reset signal can be external or internal.
 assign combined_reset_signal = internal_reset || external_reset;
+// Inverted Serdata is used to block serdata outputs while shifting
+// new target data into the module
+assign inverted_serdata_enable = ~sr_div_data_enable;
 // Adjust the target value to take into account the fact that the
 // system has to propigate through
 assign compensated_target = divider_target - divide_delay;
--- tb_divider.v
+++ tb_divider.v
@@ -5,30 +5,86 @@ reg tb_divider_reset;
 reg tb_enable_signal;
 wire tb_dut_output;
 
+// Shift register specific components of the divider
+reg tb_sr_data;
+reg tb_sr_clk;
+reg tb_div_sr_en;
+reg tb_div_sr_rst;
+
+// Integer for shifting data in
+integer counter;
+
+// Temp variable to hold test divisor
+reg [31:0] tb_div_test_target;
+
 divider dut(.pulse_clock(tb_pulse),
             .enable(tb_enable_signal),
             .external_reset(tb_divider_reset),
+            .sr_data(tb_sr_data), .sr_data_clock(tb_sr_clk),
+            .sr_div_data_enable(tb_div_sr_en),
+            .sr_div_data_reset(tb_div_sr_rst), 
             .divided_clock(tb_dut_output));
 
-// Setup clock
 always #1 tb_pulse <= ~tb_pulse;
 
 initial begin
-    // Setup initial conditions
-    // Check initialization state to see if enable behavior is correct
-    tb_pulse <= 0;
-    tb_divider_reset <= 0;
-    tb_enable_signal <= 0;
-    #20;
-    // Test divider behavior normally
+     // Initialize test variables
+     tb_pulse <= 0;
+     tb_divider_reset <= 0;
+     tb_enable_signal <= 0;
+     tb_sr_data <= 0;
+     tb_sr_clk <= 0;
+     tb_div_sr_en <= 0;
+     tb_div_sr_rst <= 0;
+     #2;
+    // 1st target is n/20
+    tb_div_test_target <= 32'd20;
+    tb_div_sr_en <= 1;
+    #2;
+    // Shift target value into the divider SR
+    for(counter = 0; counter < 32; counter = counter + 1) begin
+        tb_sr_data <= tb_div_test_target[31-counter];
+        tb_sr_clk <= ~tb_sr_clk;
+        #1;
+        tb_sr_clk <= ~tb_sr_clk;
+        #1;
+    end
+    tb_div_sr_en <= 0;
+    tb_sr_clk <= 0;
+    tb_sr_data <= 0;
     tb_enable_signal <= 1;
-    #80;
-    // Trigger reset externally
+    #2;
+    // Run for 200 cycles and check output
+    #200;
+    // Shift in new value
+    tb_div_test_target <= 32'd10;
+    tb_enable_signal <= 0;
+    tb_div_sr_en <= 1;
+    #2;
+    for(counter = 0; counter < 32; counter = counter + 1) begin
+        tb_sr_data <= tb_div_test_target[31-counter];
+        tb_sr_clk <= ~tb_sr_clk;
+        #1;
+        tb_sr_clk <= ~tb_sr_clk;
+        #1;
+    end
+    // Cleanup SR state
+    tb_div_sr_en <= 0;
+    tb_sr_clk <= 0;
+    tb_sr_data <= 0;
     tb_divider_reset <= 1;
-    #2; 
+    tb_enable_signal <= 1;
+    #2;
     tb_divider_reset <= 0;
-    #80;
+    // 200 cycles
+    #155;
+    // force a counter reset at 155 and see behavior
+    tb_divider_reset <= 1;
+    #2;
+    tb_divider_reset <= 0;
+    #43;
     $stop;
 
 end
+
 endmodule
